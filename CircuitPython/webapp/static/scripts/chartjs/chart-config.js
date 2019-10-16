0291627d956abd504cac0ada54d8684b5a9a150c
Tejas Shah <tejashah88@gmail.com>
2019-10-16 02:09:59 -0700

    removed majority of webappp-specific files

--- CircuitPython/webapp/static/scripts/chartjs/chart-config.js
+++
@@ -1,8 +0,0 @@
-Chart.defaults.global.defaultFontColor = 'white';
-
-Chart.defaults.global.responsive = true;
-Chart.defaults.global.maintainAspectRatio = true;
-
-Chart.defaults.global.elements.line.tension = 0;  // disables bezier curves
-
-const MAX_NUM_POINTS = 31;
