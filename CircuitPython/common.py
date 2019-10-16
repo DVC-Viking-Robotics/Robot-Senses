ac1ccbb897dafcb681a6c139097c54886e7a1afb
Brendan <2bndy5@gmail.com>
2019-10-16 02:56:18 -0700

    should be using pyserial builtins manage ports

--- CircuitPython/common.py
+++ CircuitPython/common.py
@@ -2,33 +2,3 @@ import board
 
 SPI_BUS = board.SPI()
 I2C_BUS = board.I2C()
-
-class Serial_Manager:
-    def __init__(self, ser_objects=None):
-        self._mgr = []
-        if ser_objects is not None:
-            for ser in ser_objects:
-                self.insert(ser)
-
-    @property
-    def get_ports(self):
-        result = []
-        for obj in self._mgr:
-            result.insert(obj.port)
-        return result
-    
-    def insert(self, obj, index=None):
-        self._mgr.insert(len(self._mgr) - 1 if index is None else index, obj)
-
-    def remove(self, port):
-        for i, obj in enumerate(self._mgr):
-            if obj.port == port:
-                self._mgr.remove(i)
-
-    def get_obj(self, port):
-        for obj in self._mgr:
-            if obj.port == port:
-                return obj
-        return None
-
-SER_MGR = Serial_Manager()
